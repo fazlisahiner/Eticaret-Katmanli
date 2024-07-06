09a57f2a8a984df9214ec25ee4f47a1565f897d4
fazlisahiner <149615948+fazlisahiner@users.noreply.github.com>
2024-07-06 12:10:30 +0300

    Eticaret-Katmanli

---
+++ ETicaret.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using ETicaret.Core.IUnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+
+        protected readonly MyECommerceDB _commerceDb;
+
+        public UnitOfWork(MyECommerceDB db)
+        {
+            _commerceDb = db;    
+        }
+
+
+        public void Commit()
+        {
+            _commerceDb.SaveChanges();
+
+        }
+
+        public async Task CommitAsync()
+        {
+           await  _commerceDb.SaveChangesAsync();
+
+        }
+    }
+}
